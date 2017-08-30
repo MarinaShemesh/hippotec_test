02c36187110adebf58bc2ff98aa8eb816c442bf4
MarinaShemesh <marina.shemesh1@gmail.com>
2017-08-30 20:06:36 +0300

    30Aug_3

--- public/app.js
+++ public/app.js
@@ -1,13 +1,42 @@
 'use strict'
 
 const app = angular.module('flowerApp', [])
- .controller('flowerController', function($scope, $http){
+
+
+  .controller('flowerController', function($scope, $http){
+    
+    const flowerArray = [];
     
     $http.get('http://52.51.81.191:85/getFlowers').
         then(function(response) {
             $scope.list = response.data;
-            console.log($scope.data);
+            // console.log('scope.list:', $scope.list);
            
+            flowerArray.push($scope.list);
+           
+            $scope.flowers = flowerArray;
+              // console.log('scope.flowers:', $scope.flowers);
+      
         });
 
 });
+
+ app.controller('translateController', function($scope, $http){
+    
+    $http.get('http://52.51.81.191:85/getTranslate').
+        then(function(response) {
+            $scope.translate = response.data;
+            // console.log($scope.translate);
+
+        });
+
+      function translatePage(){
+        var translatedItem = this;
+
+        
+
+    };
+
+
+});
+
